e87ff26b17b348f9f867ecb17ad6bee6c8bd8510
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-28 21:56:03 +0400

    feat: add constructor with validation for SqlServerCategoriesProductsReader

--- DataFileGenerator/DataReaders/SqlServerCategoriesProductsReader.cs
+++ DataFileGenerator/DataReaders/SqlServerCategoriesProductsReader.cs
@@ -6,6 +6,17 @@ namespace DataFileGenerator.DataReaders;
 
 public class SqlServerCategoriesProductsReader : IDataSourceReader<CategoryProductRow>
 {
+    private readonly SqlConnection _connection;
+    private readonly string _viewName;
+
+    public SqlServerCategoriesProductsReader(SqlConnection? connection, string viewName)
+    {
+        if (string.IsNullOrWhiteSpace(viewName))
+            throw new ArgumentException("View name cannot be null or whitespace", nameof(viewName));
+        _connection = connection ?? throw new ArgumentNullException(nameof(connection), "Connection cannot be null");
+        _viewName = viewName;
+    }
+
     public IEnumerable<CategoryProductRow> Read()
     {
         throw new NotImplementedException();

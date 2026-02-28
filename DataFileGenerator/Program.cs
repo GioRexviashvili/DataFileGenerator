c9b7dd71bff78e97e628e59405190e33a2d993e4
Giorgi Rekhviashvili <giorgi.rexviashvili.2004@gmail.com>
2026-02-28 23:42:22 +0400

    feat: implement main program logic for exporting SQL view data to TSV file

--- DataFileGenerator/Program.cs
+++ DataFileGenerator/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace DataFileGenerator;
+﻿using DataFileGenerator.DataReaders;
+using DataFileGenerator.Exporters;
+using DataFileGenerator.FileWriters;
+using Microsoft.Data.SqlClient;
 
-class Program
+namespace DataFileGenerator;
+
+internal static class Program
 {
-    static void Main(string[] args)
+    private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        const string connectionString =
+            "Server=localhost,1433;Database=Northwind;User Id=sa;Password=Giorgigamer707;TrustServerCertificate=True;";
+
+        const string viewName = "v_categories_products";
+
+        string outputPath = "OutputFile/Products.tsv";
+
+        using var connection = new SqlConnection(connectionString);
+
+        var reader = new SqlServerReader(connection, viewName);
+        var writer = new TsvWriter(outputPath);
+        var exporter = new Exporter(reader, writer);
+
+        try
+        {
+            exporter.Export();
+            Console.WriteLine($"Export completed: {outputPath}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Export failed:");
+            Console.WriteLine(ex);
+        }
     }
 }
